9e9f83264d15e4115ec33bebab5b4c3d8128fed4
Hrishi Suthar <77166957+Hrishi127@users.noreply.github.com>
2025-01-19 20:17:09 +0530

    Delete lib directory

--- lib/Tabs/About/aboutcontroller.dart
+++
@@ -1,8 +0,0 @@
-import 'package:get/get.dart';
-import 'package:url_launcher/url_launcher.dart';
-import 'package:url_launcher/url_launcher_string.dart';
-
-class AboutController extends GetxController{
-
-
-}
